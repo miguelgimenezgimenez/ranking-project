ee5fe125e9c065aac8286967062f7cdec46784f4
Miguel <gimenezgimenezmiguel@gmail.com>
2020-09-25 13:56:41 +0200

    wip

--- JsonEncoder.py
+++ JsonEncoder.py
@@ -1,32 +1,32 @@
-import json
-from collections import Iterable
-from bson import ObjectId
-from datetime import datetime
+# import json
+# from collections import Iterable
+# from bson import ObjectId
+# from datetime import datetime
 
 
-class MongoengineEncoder(json.JSONEncoder):
-    """
-    The default JSON encoder that ships with Mongoengine (the to_json method
-    exposed on Document instances) makes some odd choices. Datetime objects 
-    are nested on a $date property, ObjectIds are nested on an $oid property, 
-    etc. It makes for a confusing interface when you're serializing obejcts for, 
-    say, a JavaScript application. 
+# class MongoengineEncoder(json.JSONEncoder):
+#     """
+#     The default JSON encoder that ships with Mongoengine (the to_json method
+#     exposed on Document instances) makes some odd choices. Datetime objects
+#     are nested on a $date property, ObjectIds are nested on an $oid property,
+#     etc. It makes for a confusing interface when you're serializing obejcts for,
+#     say, a JavaScript application.
 
-    This borrows heavily from an abandoned Flask extension called Flask-Views
-    https://github.com/brocaar/flask-views/blob/master/flask_views/db/mongoengine/json.py
-    """
+#     This borrows heavily from an abandoned Flask extension called Flask-Views
+#     https://github.com/brocaar/flask-views/blob/master/flask_views/db/mongoengine/json.py
+#     """
 
-    def default(self, obj):
-        if isinstance(obj, Iterable):
-            out = {}
-            for key in obj:
-                out[key] = getattr(obj, key)
-            return out
+#     def default(self, obj):
+#         if isinstance(obj, Iterable):
+#             out = {}
+#             for key in obj:
+#                 out[key] = getattr(obj, key)
+#             return out
 
-        if isinstance(obj, ObjectId):
-            return unicode(obj)
+#         if isinstance(obj, ObjectId):
+#             return unicode(obj)
 
-        if isinstance(obj, datetime):
-            return str(obj)
+#         if isinstance(obj, datetime):
+#             return str(obj)
 
-        return json.JSONEncoder.default(self, obj)
+#         return json.JSONEncoder.default(self, obj)
--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 # https://flask.palletsprojects.com/en/1.1.x/quickstart/
 from flask import Flask
-from src.mongoengine_jsonencoder import MongoEngineJSONEncoder
+# from src.mongoengine_jsonencoder import MongoEngineJSONEncoder
 app = Flask("rankings-api")
-app.json_encoder = MongoEngineJSONEncoder
+# app.json_encoder = MongoEngineJSONEncoder
